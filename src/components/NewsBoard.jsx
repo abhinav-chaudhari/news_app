bc0181c173f593bc07aeaaf06b6261288bdddd48
Abhinav Chaudhari <abhinavchaudhari.be.it@gmail.com>
2024-04-27 18:58:51 +0530

    update in apikey

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -6,7 +6,7 @@ const NewsBoard = ({category}) => {
     const [articles, setArticles] = useState([])
 
     useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=8ebbc2b5bfae4e77a361eeac242dde00`;
         fetch(url)
         .then(response=> response.json())
         .then(data=> setArticles(data.articles));
