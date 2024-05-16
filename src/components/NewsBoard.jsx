a6bcafc7979ca86dd9adff59b75658ecf14ddb3c
Abhinav Chaudhari <abhinavchaudhari.be.it@gmail.com>
2024-05-16 21:45:17 +0530

    changes in NewBoard.jsx

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -1,25 +1,45 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 
+// let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.VITE_API_KEY}`;
 
-const NewsBoard = ({category}) => {
-    const [articles, setArticles] = useState([])
+//         const data = await fetch(url);
+//         const json = await data.json();
+//         setArticles(json.articles)
 
-    useEffect(()=>{
-        let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=8ebbc2b5bfae4e77a361eeac242dde00`;
-        fetch(url)
-        .then(response=> response.json())
-        .then(data=> setArticles(data.articles));
-        
-    },[category])
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
+
+  const getNews = async () => {
+    const data = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`
+    );
+    const json = await data.json();
+    setArticles(json.articles);
+  };
+  useEffect(() => {
+    getNews();
+  }, [category]);
   return (
     <div>
-        <h2 className="text-center">Lastest <span className="badge bg-danger">News</span></h2>
-        {articles.map((news, index)=> {
-            return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
-        })}
+      <h2 className="text-center">
+        Lastest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles.map((news, index) => {
+        return (
+          <NewsItem
+            key={index}
+            title={news.title}
+            description={news.description}
+            src={news.urlToImage}
+            url={news.url}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default NewsBoard
+export default NewsBoard;
